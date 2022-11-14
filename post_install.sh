4f0ec08b1c49f95a2dbedab676b958f42413161e
Treacherous Donut <chipmunk@arrogantrabbit.com>
2022-11-14 01:14:43 -0800

    Another attempt at logging download progress to console during install.

--- post_install.sh
+++ post_install.sh
@@ -3,12 +3,9 @@
 CONFIGPATH=/root/.duplicacy-web
 
 if [ ! -f /root/settings.json ]; then
-
   echo "Configuration file does not exist."
   echo "Creating default one enabling listening on all interfaces"
-
   mkdir -p ${CONFIGPATH}
-
   cat >${CONFIGPATH}/settings.json <<EOF
 {
     "listening_address"     : "0.0.0.0:3875"
@@ -29,22 +26,21 @@ NETWAIT_IP=1.1.1.1
 echo "Configuring netwait to wait for ${NETWAIT_IP}"
 sysrc netwait_ip="${NETWAIT_IP}"
 
-echo "Enabling netwait and newsyslog services"
+echo "Enabling services"
 service newsyslog enable
 service netwait enable
-
-echo "Starting duplicacy updater"
 service duplicacyupd enable
-service duplicacyupd start
+service duplicacy enable
 
-echo "Waiting for duplicacy binary to download"
-while [ ! -f /usr/local/bin/duplicacy_web ]; do
-  echo ... Still waiting for duplicacy to download
-  sleep 2
-done
+echo "Downloading duplicacy"
+echo
+
+/usr/local/bin/duplicacy-updater oneoff || exit 1
 
 echo "Starting duplicacy service"
-service duplicacy enable
 service duplicacy start
+
+echo "Starting duplicacy updater"
+service duplicacyupd start
 
 echo "Ready"
